a3bca01e706b70f106b68a1d0b8b1b5ef601bfcc
Rudnei Lucas <rudnei.lucas@locaweb.com.br>
2015-08-20 09:26:00 -0300

    add branch model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819201558) do
+ActiveRecord::Schema.define(version: 20150820122447) do
+
+  create_table "admin_testes", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "branches", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
