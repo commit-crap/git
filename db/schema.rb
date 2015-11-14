8436f4d0a7f90f3d226446f5edecef7e87889c99
André Lucas <andre.miranda@locaweb.com.br>
2015-11-14 08:34:15 -0200

    rolde

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112230759) do
+ActiveRecord::Schema.define(version: 20151114100315) do
 
   create_table "branches", force: :cascade do |t|
     t.string   "name"
